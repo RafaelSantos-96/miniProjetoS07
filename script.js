945997446061039639404acedde5e1f954ca0364
RafaelSantos-96 <rafaelmartins_contato@hotmail.com>
2025-07-31 02:34:08 +0000

    Exercicio 2:
    -salvando tarefas local com localstorage.getItem e usando o jsonParce para transformar texto em array;
    -salvando as tarefas no addTask com JSON.stringify;

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 const searchInput = document.getElementById("searchInput");
 
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
 const addTask = (event) => {
   event.preventDefault();
   const newTask = taskInput.value.trim();
